1fd37080ac3e7dc30705ecef2e13f6c786c269ae
Jack Collins <jack@getgalore.com>
2018-04-26 10:03:06 -0600

    Rescue the `ActiveRecord::NoDatabaseError` so you can still create databases with rake tasks in Rails 5.2

--- lib/active_record/postgres/constraints/railtie.rb
+++ lib/active_record/postgres/constraints/railtie.rb
@@ -6,23 +6,29 @@ module ActiveRecord
         initializer 'active_record.postgres.constraints.patch_active_record' do
           ActiveSupport.on_load(:active_record) do
             AR_CAS = ::ActiveRecord::ConnectionAdapters
+            begin
+              connection = ActiveRecord::Base.connection
+              using_pg = connection.class.to_s == "#{AR_CAS}::PostgreSQLAdapter"
+              if using_pg
+                Rails.logger.info do
+                  'Applying Postgres Constraints patches to ActiveRecord'
+                end
+                AR_CAS::TableDefinition.include TableDefinition
+                AR_CAS::PostgreSQLAdapter.include PostgreSQLAdapter
+                AR_CAS::AbstractAdapter::SchemaCreation.prepend SchemaCreation
 
-            connection = ActiveRecord::Base.connection
-            using_pg = connection.class.to_s == "#{AR_CAS}::PostgreSQLAdapter"
-            if using_pg
-              Rails.logger.info do
-                'Applying Postgres Constraints patches to ActiveRecord'
+                ::ActiveRecord::Migration::CommandRecorder.include CommandRecorder
+                ::ActiveRecord::SchemaDumper.prepend SchemaDumper
+              else
+                Rails.logger.warn do
+                  'Not applying Postgres Constraints patches to ActiveRecord ' \
+                'since the database is not postgres'
+                end
               end
-              AR_CAS::TableDefinition.include TableDefinition
-              AR_CAS::PostgreSQLAdapter.include PostgreSQLAdapter
-              AR_CAS::AbstractAdapter::SchemaCreation.prepend SchemaCreation
-
-              ::ActiveRecord::Migration::CommandRecorder.include CommandRecorder
-              ::ActiveRecord::SchemaDumper.prepend SchemaDumper
-            else
+            rescue ActiveRecord::NoDatabaseError
               Rails.logger.warn do
                 'Not applying Postgres Constraints patches to ActiveRecord ' \
-                  'since the database is not postgres'
+                'because the database does not exist'
               end
             end
           end
