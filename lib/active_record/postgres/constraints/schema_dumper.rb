9207b0008cf8b4c87b45691839706010b5284abb
Felix Kenton <robotfelix@gmail.com>
2018-07-06 23:39:01 +0100

    Update SchemaDumper to work with Rails < 5.0

--- lib/active_record/postgres/constraints/schema_dumper.rb
+++ lib/active_record/postgres/constraints/schema_dumper.rb
@@ -3,17 +3,41 @@ module ActiveRecord
   module Postgres
     module Constraints
       module SchemaDumper
-        def indexes_in_create(table, stream)
-          super
-          constraints = @connection.constraints(table)
-          return unless constraints.any?
-          constraint_statements = constraints.map do |constraint|
-            name = constraint['conname']
-            conditions = constraint['consrc']
-            "    t.check_constraint :#{name}, #{conditions.inspect}"
+
+        if ActiveRecord::SchemaDumper.private_instance_methods(false).include?(:indexes_in_create)
+          # Rails >= 5.0
+          def indexes_in_create(table, stream)
+            super
+            constraints = @connection.constraints(table)
+            return unless constraints.any?
+            constraint_statements = constraints.map do |constraint|
+              name = constraint['conname']
+              conditions = constraint['consrc']
+              "    t.check_constraint :#{name}, #{conditions.inspect}"
+            end
+            stream.puts constraint_statements.sort.join("\n")
           end
-          stream.puts constraint_statements.sort.join("\n")
+        else
+          # Rails < 5.0 does not have hook for table creation so add separate statements
+          def indexes(table, stream)
+            super
+            constraints = @connection.constraints(table)
+            return unless constraints.any?
+            constraint_statements = constraints.map do |constraint|
+              name = constraint['conname']
+              conditions = constraint['consrc']
+              statement_parts = [
+                  "add_check_constraint #{remove_prefix_and_suffix(table).inspect}",
+                  "#{name.inspect}",
+                  "#{conditions.inspect}"
+              ]
+              "  #{statement_parts.join(', ')}"
+            end
+            stream.puts constraint_statements.sort.join("\n")
+            stream.puts
+          end
         end
+
       end
     end
   end
