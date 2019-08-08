1a178c01951e9d2b1ed06dc8817fa70552e4d972
Isaac Betesh <iybetesh@gmail.com>
2019-08-08 13:47:52 -0700

    Move all check-constraint specific code and tests into a new directory structure in preparation for additional namespacing
    This commit contains no changes to the code other than moving it

---
+++ lib/active_record/postgres/constraints/types/check.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+module ActiveRecord
+  module Postgres
+    module Constraints
+      class << self
+        def normalize_conditions(conditions)
+          conditions = [conditions] unless conditions.is_a?(Array)
+          conditions = conditions.map do |condition|
+            if condition.is_a?(Hash)
+              normalize_conditions_hash(condition)
+            else
+              condition
+            end
+          end
+
+          return conditions.first if 1 == conditions.length
+
+          "(#{conditions.join(') AND (')})"
+        end
+
+        def normalize_conditions_hash(hash)
+          hash = hash.reduce([]) do |array, (column, predicate)|
+            predicate = predicate.join("', '") if predicate.is_a?(Array)
+            array << "#{column} IN ('#{predicate}')"
+          end
+          "(#{hash.join(') AND (')})"
+        end
+
+        def to_sql(table, name_or_conditions, conditions = nil)
+          if conditions
+            name = name_or_conditions
+          else
+            name = "#{table}_#{Time.zone.now.nsec}"
+            conditions = name_or_conditions
+          end
+
+          "CONSTRAINT #{name} CHECK (#{normalize_conditions(conditions)})"
+        end
+      end
+    end
+  end
+end
